663342ac4e205bf36a52e832f12b8b113d5f755d
Alexander Hingtgen <ahingtgen68@gmail.com>
2022-04-04 13:55:20 -0500

    vtm_site initial commit

--- app.py
+++ app.py
@@ -5,11 +5,26 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', pageTitle='Vertical Tank Maintenance')
 
-@app.route('/mike')
-def mike():
-    return render_template('mike.html')
+@app.route('/about')
+def about():
+    return render_template('about.html', pageTitle='About Vertical Tank Maintenance')
+
+@app.route('/estimate', methods=['GET','POST'])
+def estimate():
+    if request.method == 'POST':
+        form = request.form
+        radius = float(form['radius'])
+        height = float(form['height'])
+        tank_top = pi * radius**2
+        tank_side = 2 * (pi * (radius * height))
+        tank_area = tank_top + tank_side
+        total_sqft = tank_area / 144
+        material_cost = total_sqft * 25
+        labor_cost = total_sqft * 15
+        estimate = "${:,.2f}".format(round(material + labor, 2))
+    return render_template('estimate.html', pageTitle='VTM Estimator')
 
 if __name__ == '__main__':
     app.run(debug=True)
