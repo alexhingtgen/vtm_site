50998f7158400629d847016262d44f8d7c8947e4
Alexander Hingtgen <ahingtgen68@gmail.com>
2022-04-04 15:37:07 -0500

    estimate page returns correct value

--- app.py
+++ app.py
@@ -18,13 +18,16 @@ def estimate():
         form = request.form
         radius = float(form['radius'])
         height = float(form['height'])
+        pi = 3.14
         tank_top = pi * radius**2
+        print(tank_top)
         tank_side = 2 * (pi * (radius * height))
+        print(tank_side)
         tank_area = tank_top + tank_side
         total_sqft = tank_area / 144
         material_cost = total_sqft * 25
         labor_cost = total_sqft * 15
-        total_estimate = "${:,.2f}".format(round(material + labor, 2))
+        total_estimate = "${:,.2f}".format(round(material_cost + labor_cost, 2))
         return render_template('estimate.html', estimate = total_estimate)
     return render_template('estimate.html', pageTitle='VTM Estimator')
 
