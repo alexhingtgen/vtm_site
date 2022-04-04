68f8cd7b3ae456642e6dbee1cdd4903fa882e322
Alexander Hingtgen <ahingtgen68@gmail.com>
2022-04-04 14:40:57 -0500

    index page properly formatted

--- app.py
+++ app.py
@@ -13,7 +13,18 @@ def about():
 
 @app.route('/estimate', methods=['GET','POST'])
 def estimate():
-    return render_template('estimate.html', pageTitle='VTM Estimator')
+    if request.method == 'POST':
+        form = request.form
+        radius = float(form['radius'])
+        height = float(form['height'])
+        tank_top = pi * radius**2
+        tank_side = 2 * (pi * (radius * height))
+        tank_area = tank_top + tank_side
+        total_sqft = tank_area / 144
+        material_cost = total_sqft * 25
+        labor_cost = total_sqft * 15
+        total_estimate = "${:,.2f}".format(round(material + labor, 2))
+    return render_template('estimate.html', pageTitle='VTM Estimator', estimate = total_estimate)
 
 if __name__ == '__main__':
     app.run(debug=True)
